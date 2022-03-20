d22add9d19471f36b950f5a80f369f385e4b4caf
Bello Musa <musbell007@gmail.com>
2022-03-20 23:35:45 +0100

    chore(prismaservice): remove unuse prismaService in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
 import { TodosModule } from './todos/todos.module';
-import { PrismaService } from '../prisma/prisma.service';
 import { GraphQLDateTime } from 'graphql-iso-date';
 
 @Module({
